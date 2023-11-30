985b63b822b3524c76748b053296f79f31d6fd16
haocheng <lhao_cheng@163.com>
2023-11-30 14:13:00 +0800

    change update and remove unused file

--- analyze/views.py
+++ analyze/views.py
@@ -5,6 +5,8 @@ from django.core.files.storage import default_storage, Storage
 import os
 import sqlite3
 from django.core.files.uploadedfile import InMemoryUploadedFile
+from . import settings
+
 
 def hello(request):
     return HttpResponse("Hello world ! ")
@@ -27,29 +29,38 @@ def insert_database(model_file, platform):
     conn.close()
 
 def save_file(model_file, platform):
-    # 计算文件大小
-    file_size_bytes = model_file.size
-    file_size_kb = file_size_bytes / 1024
-    file_size_mb = file_size_kb / 1024
-
-    # 创建文件大小信息字符串
-    file_size_info = f'文件大小：{file_size_mb:.2f} MB'
-
-    # 将文件大小信息写入到文件中
-    # name, ext = os.path.splitext(model_file.name)
-    # result_file = "upload/" + name + "_result_" + ext
-    save_path = "upload/" + model_file.name
-    filename = os.path.join(save_path)
+    save_path = os.path.join(settings.MEDIA_ROOT, model_file.name)
+    with open(save_path, 'wb') as destination:
+        for chunk in model_file.chunks():
+            destination.write(chunk)
 
     insert_database(model_file, platform)
 
-    with open(filename, 'w') as f:
-        f.write(file_size_info)
+# def progress(model_file, platform):
+#     file_size_bytes = model_file.size
+#     file_size_kb = file_size_bytes / 1024
+#     file_size_mb = file_size_kb / 1024
+#
+#     file_size_info = f'文件大小：{file_size_mb:.2f} MB'
+#
+#     # 将文件大小信息写入到文件中
+#     # name, ext = os.path.splitext(model_file.name)
+#     # result_file = "upload/" + name + "_result_" + ext
+#     save_path = "upload/" + model_file.name
+#     filename = os.path.join(save_path)
+#
+#     with open(filename, 'w') as f:
+#         f.write(file_size_info)
+
+
+
 
 def create_task(request):
     if request.method == 'POST':
         model_file = request.FILES['modelFile']
         platform = request.POST.get('platform')
+
         save_file(model_file, platform)
+        #progress(model_file, platform)
 
     return HttpResponse('task completed')
