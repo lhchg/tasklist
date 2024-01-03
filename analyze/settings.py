6da7e30a8b646fc3743852b11bf00e377bd010b3
Haocheng Li <haocheng1.li@samsung.com>
2024-01-03 16:18:52 +0800

    add ip

--- analyze/settings.py
+++ analyze/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-gsee!110nn3aoxpz&5p86vz$(07s_e5si&2&k3kch8(&1=fduc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['109.144.2.161']
+ALLOWED_HOSTS = ['109.144.2.161', '109.144.2.169']
 MODEL_ROOT = os.path.join(BASE_DIR, 'upload')
 #MEDIA_URL = '/upload/'
 RESULT_ROOT = os.path.join(BASE_DIR, 'result')
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
